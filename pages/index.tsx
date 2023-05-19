18966182ad863a8337554bf65aab7ae06c274b64
Admin <support@unlimitpotential.com>
2023-05-19 16:18:22 -0400

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Box, Code, Text, VStack, Grid } from '@chakra-ui/react'
 import { Layout } from '../components/Layout'
 import { Nextjs } from '../components/NextLogo'
 import { Link } from '@saas-ui/react'
+import Blog from './blog';
 
 const IndexPage = () => {
   return (
@@ -9,20 +10,8 @@ const IndexPage = () => {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <VStack spacing={8}>
-            <Nextjs h="16" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">pages/index.tsx</Code> and save to
-              reload.
-            </Text>
-            <Link
-              color="purple.500"
-              fontSize="xl"
-              href="https://saas-ui.dev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn more about Saas UI
-            </Link>
+           
+           <Blog mediumArticles={undefined}/>
           </VStack>
         </Grid>
       </Box>
