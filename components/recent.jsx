0be8dcc7bb25834138a8311d2561ef1357d21315
Admin <support@unlimitpotential.com>
2023-05-19 00:01:14 -0400

    g

--- components/recent.jsx
+++ components/recent.jsx
@@ -1,11 +1,11 @@
-import Section 		from '../../structure/section';
-import Container 	from '../../structure/container';
+import Section 		from '../structure/section';
+import Container 	from '../structure/container';
 
 import Image from 'next/image'
 import SectionTitle from './blocks/section.title.block'
 
 
-import css from '../../../styles/sections/articles/recent.module.scss'
+import css from '../../styles/sections/articles/recent.module.scss'
 
 export default function Recent({ mediumArticles }) {
 
