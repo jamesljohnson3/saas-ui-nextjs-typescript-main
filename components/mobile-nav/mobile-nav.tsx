3426595fa69fe992991540e8f8306dcb794fdf03
Admin <support@unlimitpotential.com>
2023-05-19 16:56:19 -0400

    s

--- components/mobile-nav/mobile-nav.tsx
+++ components/mobile-nav/mobile-nav.tsx
@@ -1,61 +1,57 @@
 import {
   Box,
-  CloseButton,
+  BoxProps,
+  Center,
+  CenterProps,
   Flex,
+  Grid,
+  GridItem,
   HStack,
   IconButton,
   IconButtonProps,
-  LinkProps,
-  Stack,
-  useBreakpointValue,
   useColorModeValue,
-  useUpdateEffect,
 } from '@chakra-ui/react'
 import { AnimatePresence, motion } from 'framer-motion'
-import useRouteChanged from '../../hooks/use-route-changed'
-// import { getRoutes } from '@/layouts/mdx'
+import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import * as React from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { RemoveScroll } from 'react-remove-scroll'
+import Logo from '../../components/layouts/logo'
+import { SidebarContent } from '../sidebar/sidebar'
 
-import siteConfig from '../../data/config'
-import Logo  from '../../components/layouts/logo'
-import { Link } from '@saas-ui/react'
+import headerNav from '../../data/header-nav'
 
-interface NavLinkProps extends LinkProps {
+interface NavLinkProps extends CenterProps {
   label: string
   href?: string
   isActive?: boolean
 }
 
-function NavLink({ href, children, isActive, ...rest }: NavLinkProps) {
-  const { pathname } = useRouter()
+function NavLink({ href, children, label, isActive, ...rest }: NavLinkProps) {
   const bgActiveHoverColor = useColorModeValue('gray.100', 'whiteAlpha.100')
 
-  const [, group] = href?.split('/') || []
-  isActive = isActive ?? pathname.includes(group)
-
+ 
   return (
-    <Link
-      href={href}
-      display="inline-flex"
-      flex="1"
-      minH="40px"
-      px="8"
-      py="3"
-      transition="0.2s all"
-      fontWeight={isActive ? 'semibold' : 'medium'}
-      borderColor={isActive ? 'purple.400' : undefined}
-      borderBottomWidth="1px"
-      color={isActive ? 'white' : undefined}
-      _hover={{
-        bg: isActive ? 'purple.500' : bgActiveHoverColor,
-      }}
-      {...rest}
-    >
-      {children}
-    </Link>
+    <GridItem as={NextLink} href={href}>
+      <Center
+        flex="1"
+        minH="40px"
+        as="button"
+        rounded="md"
+        transition="0.2s all"
+        fontWeight={isActive ? 'semibold' : 'medium'}
+        borderColor={isActive ? 'purple.400' : undefined}
+        borderWidth="1px"
+        color={isActive ? 'white' : undefined}
+        _hover={{
+          bg: isActive ? 'purple.500' : bgActiveHoverColor,
+        }}
+        {...rest}
+      >
+        {label}
+      </Center>
+    </GridItem>
   )
 }
 
@@ -65,32 +61,16 @@ interface MobileNavContentProps {
 }
 
 export function MobileNavContent(props: MobileNavContentProps) {
-  const { isOpen, onClose = () => {} } = props
-  const closeBtnRef = React.useRef<HTMLButtonElement>(null)
+  const { isOpen } = props
+  const { pathname } = useRouter()
   const bgColor = useColorModeValue('whiteAlpha.900', 'blackAlpha.900')
 
-  useRouteChanged(onClose)
 
   /**
    * Scenario: Menu is open on mobile, and user resizes to desktop/tablet viewport.
    * Result: We'll close the menu
    */
-  const showOnBreakpoint = useBreakpointValue({ base: true, lg: false })
-
-  React.useEffect(() => {
-    if (showOnBreakpoint == false) {
-      onClose()
-    }
-  }, [showOnBreakpoint, onClose])
-
-  useUpdateEffect(() => {
-    if (isOpen) {
-      requestAnimationFrame(() => {
-        closeBtnRef.current?.focus()
-      })
-    }
-  }, [isOpen])
-
+ 
   return (
     <AnimatePresence>
       {isOpen && (
@@ -107,10 +87,10 @@ export function MobileNavContent(props: MobileNavContentProps) {
               bg={bgColor}
               h="100vh"
               overflow="auto"
-              pos="absolute"
+              position="fixed"
               top="0"
               left="0"
-              zIndex="modal"
+              zIndex="sticky"
               pb="8"
               backdropFilter="blur(5px)"
             >
@@ -118,25 +98,39 @@ export function MobileNavContent(props: MobileNavContentProps) {
                 <Flex justify="space-between" px="8" pt="4" pb="4">
                   <Logo />
                   <HStack spacing="5">
-                    <CloseButton ref={closeBtnRef} onClick={onClose} />
+                   X
                   </HStack>
                 </Flex>
-                <Stack alignItems="stretch" spacing="0">
-                  {siteConfig.header.links.map(
-                    ({ href, id, label, ...props }, i) => {
+                <Grid
+                  px="6"
+                  pb="6"
+                  pt="2"
+                  templateColumns="repeat(2, 1fr)"
+                  gap="2"
+                >
+                  {headerNav.map(
+                    ({ href, id, title, colorScheme, ...props }, i) => {
                       return (
                         <NavLink
                           href={href || `/#${id}`}
                           key={i}
                           {...(props as any)}
                         >
-                          {label}
+                          {title}
                         </NavLink>
                       )
                     }
                   )}
-                </Stack>
+                </Grid>
               </Box>
+
+              <ScrollView
+                
+              >
+                <SidebarContent
+                  pathname={pathname}
+                />
+              </ScrollView>
             </Flex>
           </motion.div>
         </RemoveScroll>
@@ -145,21 +139,37 @@ export function MobileNavContent(props: MobileNavContentProps) {
   )
 }
 
-export const MobileNavButton = React.forwardRef(
+const ScrollView = (props: BoxProps & { onScroll?: any }) => {
+  const { onScroll, ...rest } = props
+
+
+
+  return (
+    <Box
+      flex="1"
+      id="routes"
+      overflow="auto"
+      px="6"
+      pb="6"
+      {...rest}
+    />
+  )
+}
+
+export const MobileNavButton = 
   (props: IconButtonProps, ref: React.Ref<any>) => {
     return (
       <IconButton
         ref={ref}
-        display={{ base: 'flex', md: 'none' }}
+        display={{ base: 'flex', lg: 'none' }}
         fontSize="20px"
         color={useColorModeValue('gray.800', 'inherit')}
         variant="ghost"
         icon={<AiOutlineMenu />}
         {...props}
-        aria-label="Open menu"
       />
     )
   }
-)
+
 
 MobileNavButton.displayName = 'MobileNavButton'
