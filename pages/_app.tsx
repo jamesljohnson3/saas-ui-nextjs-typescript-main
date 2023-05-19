5908de9531674cb7e3d96385b016c22c367dbd05
Admin <support@unlimitpotential.com>
2023-05-19 19:33:53 -0400

    s

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,42 @@
-export interface AppRenderProps {
-  pageProps: object
-  err?: Error
-  Component: NextComponentType<NextPageContext, AppRenderProps, object>
-  router: NextRouter
-}
-import type { NextComponentType, NextPageContext } from 'next'
-import type { NextRouter } from 'next/router'
-import Link from 'next/link'
-import Layout from '../components/layouts'
-import { AppProps } from 'next/app'
-import {Footer} from '../components/layouts/footer'
-
-import { LazyMotion, domAnimation } from "framer-motion"
-
-import { SaasProvider } from '@saas-ui/react'
-import '../styles/css/variables.css'
-import '../styles/css/global.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { LazyMotion, domAnimation } from "framer-motion";
+import { SaasProvider } from '@saas-ui/react';
+import { AppProps } from 'next/app';
+import { Footer } from '../components/layouts';
+import Link from 'next/link';
+import Layout from '../components/layouts';
+import '../styles/css/variables.css';
+import '../styles/css/global.css';
 import '../styles/css/scrollbar.css';
 import "../styles/globals.css";
+import { builder } from '@builder.io/react';
 
-import { builder } from '@builder.io/react'
+builder.init('a480daf8d0e34c12bcc3f48f0079ed71');
 
-builder.init('a480daf8d0e34c12bcc3f48f0079ed71')
+const theme = extendTheme({
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
+});
 
 const MyApp = ({ Component, pageProps }: AppProps<any>) => {
   return (
-    <SaasProvider linkComponent={Link}>
-         <Layout
-        announcement={pageProps.announcement}
-        header={pageProps.header} footer={undefined}
-          >
-               
-       <LazyMotion features={domAnimation}>
-      <Component {...pageProps} /></LazyMotion><Footer /></Layout>
-    </SaasProvider>
-  )
-}
-export default MyApp
+    <ChakraProvider theme={theme}>
+      <SaasProvider linkComponent={Link}>
+        <Layout
+          announcement={pageProps.announcement}
+          header={pageProps.header}
+          footer={undefined}
+        >
+          <LazyMotion features={domAnimation}>
+            <Component {...pageProps} />
+          </LazyMotion>
+          <Footer />
+        </Layout>
+      </SaasProvider>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
