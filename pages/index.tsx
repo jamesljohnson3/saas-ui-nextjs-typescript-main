80d12774d16020ad4846574d4349f5ff1a8016fb
Admin <support@unlimitpotential.com>
2023-05-19 16:31:47 -0400

    u

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import { Box, VStack, Grid } from '@chakra-ui/react'
 import { Layout } from '../components/Layout'
+import Recent 		from '../components/recent'
+import settings 	from '../content/_settings.json'
 
-const IndexPage = () => {
+const IndexPage = ({ mediumArticles }) => {
   return (
     <Layout title="Saas UI + Next.js + TypeScript example">
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <VStack spacing={8}>
            
-    
+          <Recent mediumArticles={mediumArticles}/>
           </VStack>
         </Grid>
       </Box>
@@ -16,3 +18,23 @@ const IndexPage = () => {
   )
 }
 export default IndexPage
+// This gets called on every request
+export async function getServerSideProps({ res }) {
+
+	res.setHeader(
+		'Cache-Control',
+		'public, s-maxage=600, stale-while-revalidate=59'
+	)
+
+	console.log(settings.username.medium)
+
+	const [ mediumRSS ] = await Promise.all( [
+		fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`),
+	] )
+	
+	let [ mediumArticles ] = await Promise.all( [
+		mediumRSS.json(),
+	] )
+
+	return { props: { mediumArticles } }
+}
