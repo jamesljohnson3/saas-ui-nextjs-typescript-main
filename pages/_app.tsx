4eb10095a76382732d0e4d38cac2d4b875cda8e3
Admin <support@unlimitpotential.com>
2023-05-19 15:25:38 -0400

    f

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,18 +7,13 @@ export interface AppRenderProps {
 import type { NextComponentType, NextPageContext } from 'next'
 import type { NextRouter } from 'next/router'
 import Link from 'next/link'
-import { LazyMotion, domAnimation } from "framer-motion"
 
 import { SaasProvider } from '@saas-ui/react'
-import '../styles/css/variables.css'
-import '../styles/css/global.css'
-import '../styles/css/scrollbar.css';
 
 export default function App({ Component, pageProps }: AppRenderProps) {
   return (
     <SaasProvider linkComponent={Link}>
-      <LazyMotion features={domAnimation}>
-      <Component {...pageProps} /></LazyMotion>
+      <Component {...pageProps} />
     </SaasProvider>
   )
 }
