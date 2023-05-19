1db748c9caa6c89a9e5039cb5c066fb15ceede10
Admin <support@unlimitpotential.com>
2023-05-19 19:18:03 -0400

    s

--- pages/blank.tsx
+++ pages/blank.tsx
@@ -1,5 +1,7 @@
-import { Box, VStack, Grid, Flex, Link, Stack, chakra } from '@chakra-ui/react'
+import { Box, Code, Text, VStack, Grid } from '@chakra-ui/react'
 import { Layout } from '../components/Layout'
+import { Nextjs } from '../components/NextLogo'
+import { Link } from '@saas-ui/react'
 
 const IndexPage = () => {
   return (
@@ -7,132 +9,21 @@ const IndexPage = () => {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <VStack spacing={8}>
-          <Flex
-  bg="#edf3f8"
-  _dark={{
-    bg: "#3e3e3e",
-  }}
-  p={50}
-  w="full"
-  alignItems="center"
-  justifyContent="center"
->
-  <Box
-    bg="gray.50"
-    _dark={{
-      bg: "gray.800",
-    }}
-  >
-    <Box
-      maxW="7xl"
-      w={{
-        md: "3xl",
-        lg: "4xl",
-      }}
-      mx="auto"
-      py={{
-        base: 12,
-        lg: 16,
-      }}
-      px={{
-        base: 4,
-        lg: 8,
-      }}
-      display={{
-        lg: "flex",
-      }}
-      alignItems={{
-        lg: "center",
-      }}
-      justifyContent={{
-        lg: "space-between",
-      }}
-    >
-      <chakra.h2
-        fontSize={{
-          base: "3xl",
-          sm: "4xl",
-        }}
-        fontWeight="extrabold"
-        letterSpacing="tight"
-        lineHeight="shorter"
-        color="gray.900"
-        _dark={{
-          color: "gray.100",
-        }}
-      >
-        <chakra.span display="block">Ready to dive in?</chakra.span>
-        <chakra.span
-          display="block"
-          color="brand.600"
-          _dark={{
-            color: "gray.500",
-          }}
-        >
-          Start your free trial today.
-        </chakra.span>
-      </chakra.h2>
-      <Stack
-        direction={{
-          base: "column",
-          sm: "row",
-        }}
-        mt={{
-          base: 8,
-          lg: 0,
-        }}
-        flexShrink={{
-          lg: 0,
-        }}
-      >
-        <Link
-          display="inline-flex"
-          alignItems="center"
-          justifyContent="center"
-          px={5}
-          py={3}
-          border="solid transparent"
-          fontWeight="bold"
-          rounded="md"
-          shadow="md"
-          _light={{
-            color: "white",
-          }}
-          bg="brand.600"
-          _dark={{
-            bg: "brand.500",
-          }}
-          _hover={{
-            bg: "brand.700",
-            _dark: {
-              bg: "brand.600",
-            },
-          }}
-        >
-          Get started
-        </Link>
-        <Link
-          display="inline-flex"
-          alignItems="center"
-          justifyContent="center"
-          px={5}
-          py={3}
-          border="solid transparent"
-          fontWeight="bold"
-          rounded="md"
-          shadow="md"
-          color="brand.600"
-          bg="white"
-          _hover={{
-            bg: "brand.50",
-          }}
-        >
-          Learn More
-        </Link>
-      </Stack>
-    </Box>
-  </Box>
-</Flex>   </VStack>
+            <Nextjs h="16" pointerEvents="none" />
+            <Text>
+              Edit <Code fontSize="xl">pages/index.tsx</Code> and save to
+              reload.
+            </Text>
+            <Link
+              color="purple.500"
+              fontSize="xl"
+              href="https://saas-ui.dev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn more about Saas UI
+            </Link>
+          </VStack>
         </Grid>
       </Box>
     </Layout>
