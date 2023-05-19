f0783b9359feb6e76cb75c25cf500d3adf761ecc
Admin <support@unlimitpotential.com>
2023-05-19 18:36:20 -0400

    j

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,11 @@ import { SaasProvider } from '@saas-ui/react'
 import '../styles/css/variables.css'
 import '../styles/css/global.css'
 import '../styles/css/scrollbar.css';
+import "../styles/globals.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 
 const MyApp = ({ Component, pageProps }: AppProps<any>) => {
   return (
