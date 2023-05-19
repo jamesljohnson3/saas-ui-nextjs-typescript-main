459184ee0772ebf96f3154aab4fce9bcbef6e201
Admin <support@unlimitpotential.com>
2023-05-19 01:01:17 -0400

    u

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,28 +5,27 @@ import { Link } from '@saas-ui/react'
 
 const IndexPage = () => {
   return (
-    <Layout title="Saas UI + Next.js + TypeScript example">
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <VStack spacing={8}>
-            <Nextjs h="16" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">pages/index.tsx</Code> and save to
-              reload.
-            </Text>
-            <Link
-              color="purple.500"
-              fontSize="xl"
-              href="https://saas-ui.dev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn more about Saas UI
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </Layout>
+    <><Layout /><Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <VStack spacing={8}>
+          <Nextjs h="16" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">pages/index.tsx</Code> and save to
+            reload.
+          </Text>
+          <Link
+            color="purple.500"
+            fontSize="xl"
+            href="https://saas-ui.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn more about Saas UI
+          </Link>
+        </VStack>
+      </Grid>
+    </Box></>
+
   )
 }
 export default IndexPage
