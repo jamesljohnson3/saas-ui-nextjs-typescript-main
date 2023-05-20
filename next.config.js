2a57f85e8f3d2340dac7a7fb0378146cb7edb2dd
Admin <support@unlimitpotential.com>
2023-05-19 23:57:01 -0400

    s

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['picsum.photos', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com','res.cloudinary.com'],
+    },
+};
+
+module.exports = nextConfig;
