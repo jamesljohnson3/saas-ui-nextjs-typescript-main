93b64c25a59d03b814c7968c14b5cfd32151c4a5
Admin <support@unlimitpotential.com>
2023-05-20 01:43:50 -0400

    g

--- pages/testui.tsx
+++ pages/testui.tsx
@@ -1,43 +1,43 @@
+import { Field, FormLayout, StepForm, FormStep, NextButton } from '@saas-ui/react';
 
-import { Field, FormLayout} from '@saas-ui/react'
-import {
-  StepForm,
-  FormStep,
-  NextButton,
-} from '@saas-ui/react'
+export default function Home() {
+  const onSubmit = (params: any) => {
+    console.log(params);
+    return new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+  };
 
-export default function Home () {
-    const onSubmit = (params: any) => {
-     console.log(params)
-     return new Promise((resolve) => {
-       setTimeout(resolve, 1000)
-     })
-   }
-return ( <><StepForm
-    defaultValues={{
-      name: '',
-      email: '',
-      password: '',
-    }}
-    onSubmit={onSubmit}
-  >
-    <FormLayout>
-      <FormStep name="profile">
+  return (
+    <>
+      <StepForm
+        defaultValues={{
+          name: '',
+          email: '',
+          password: '',
+        }}
+        onSubmit={onSubmit}
+      >
         <FormLayout>
-          <Field name="name" label="Name" rules={{ required: true }} />
-          <Field name="email" label="Email" rules={{ required: true }} />
-          <NextButton />
+          <FormStep name="profile">
+            <FormLayout>
+              <Field name="name" label="Name" rules={{ required: true }} />
+              <Field name="email" label="Email" rules={{ required: true }} />
+              <NextButton />
+            </FormLayout>
+          </FormStep>
+          <FormStep name="password">
+            <FormLayout>
+              <Field
+                name="password"
+                label="Password"
+                rules={{ required: true, minLength: 4 }}
+              />
+              <NextButton />
+            </FormLayout>
+          </FormStep>
         </FormLayout>
-      </FormStep>
-      <FormStep name="password">
-        <FormLayout>
-          <Field
-            name="password"
-            label="Password"
-            rules={{ required: true, minLength: 4 }}
-          />
-          <NextButton />
-        </FormLayout>
-      </FormStep>
-    </FormLayout>
-  </StepForm></>)} 
+      </StepForm>
+    </>
+  );
+}
