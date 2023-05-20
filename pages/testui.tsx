0f5593885583bc0f3fbbfe150dc48adcf7a9d501
Admin <support@unlimitpotential.com>
2023-05-20 01:59:51 -0400

    s

--- pages/testui.tsx
+++ pages/testui.tsx
@@ -4,7 +4,20 @@ import {
     SidebarSection,
     NavItem,
   } from '@saas-ui/sidebar'
-  import { Box } from '@chakra-ui/react'
+  import { Box, Text,  SimpleGrid,
+  } from '@chakra-ui/react'
+
+  import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Button,
+  Link,
+  CardFooter,
+} from '@saas-ui/react'
+
+
 export default function Home() {
 
 
@@ -25,8 +38,45 @@ Logo    </Box>
   }
 >
   <Box as="main" flex="1" py="2" px="4" overflowY="auto">
-    Your application content
-  </Box>
+  <SimpleGrid columns={[1, null, 2]} gap="2">
+  <Card variant="outline">
+    <CardHeader>
+
+Logo      <CardTitle>Github</CardTitle>
+    </CardHeader>
+    <CardBody py={2}>
+      <Text color="muted" size="md">
+        Link pull requests
+      </Text>
+    </CardBody>
+    <CardFooter spacing={4}>
+      <Button variant="outline" colorScheme="green">
+        Enabled
+      </Button>
+      <Button as={Link} variant="link" isExternal>
+        Learn more
+      </Button>
+    </CardFooter>
+  </Card>
+  <Card variant="outline">
+    <CardHeader>
+Logo      <CardTitle>Slack</CardTitle>
+    </CardHeader>
+    <CardBody py={2}>
+      <Text color="muted" size="md">
+        Send push notifications
+      </Text>
+    </CardBody>
+    <CardFooter spacing={4}>
+      <Button colorScheme="gray" variant="solid">
+        Enable
+      </Button>
+      <Button as={Link} variant="link" isExternal>
+        Learn more
+      </Button>
+    </CardFooter>
+  </Card>
+</SimpleGrid>  </Box>
 </AppShell>
     </>
   );
