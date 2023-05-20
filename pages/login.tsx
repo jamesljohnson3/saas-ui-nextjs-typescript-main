acef9b291385123d563ab4169bd17cfbd7f5e6c2
Admin <support@unlimitpotential.com>
2023-05-19 23:15:24 -0400

    d

---
+++ pages/login.tsx
@@ -0,0 +1,21 @@
+import { Center } from '@chakra-ui/react'
+import { BackgroundGradient } from '../components/gradients/background-gradient'
+import { PageTransition } from '../components/motion/page-transition'
+import { Section } from '../components/section'
+import { NextPage } from 'next'
+
+const Login: NextPage = () => {
+  return (
+    <Section height="calc(100vh - 200px)" innerWidth="container.sm">
+      <BackgroundGradient zIndex="-1" />
+
+      <Center height="100%" pt="20">
+        <PageTransition width="100%">
+         
+        </PageTransition>
+      </Center>
+    </Section>
+  )
+}
+
+export default Login
