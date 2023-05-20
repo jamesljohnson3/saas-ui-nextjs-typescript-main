0462f3ce3ce190a4fc9a0728101ad3a24d056605
Admin <support@unlimitpotential.com>
2023-05-20 01:39:40 -0400

    ss

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -2,13 +2,23 @@ import { NextPage } from 'next'
 import NextLink from 'next/link'
 import { Box, Center, Stack, Text } from '@chakra-ui/react'
 import {  Link } from '@saas-ui/react'
-import { Features } from '../components/features'
 import { BackgroundGradient } from '../components/gradients/background-gradient'
 import { Section } from '../components/section'
 import siteConfig from '../data/config'
 
 import { PageTransition } from '../components/motion/page-transition'
-
+const people = [
+  {
+    name: 'Lindsay Walton',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=256&h=256&q=80',
+  },
+  // More people...
+]
+const activityItems = [
+  { id: 1, person: people[0], project: 'Workcation', commit: '2d89f0c8', environment: 'production', time: '1h' },
+  // More items...
+]
 
 const Login: NextPage = () => {
   return (
@@ -40,21 +50,26 @@ const Login: NextPage = () => {
                 mb={{ base: 0, lg: 16 }}
               />
             </NextLink>
-            <Features
-              display={{ base: 'none', lg: 'flex' }}
-              columns={1}
-              iconSize={4}
-              flex="1"
-              py="0"
-              ps="0"
-              maxW={{ base: '100%', xl: '80%' }}
-              features={siteConfig.signup.features.map((feature) => ({
-                iconPosition: 'left',
-                variant: 'left-icon',
-
-                ...feature,
-              }))}
-            />
+            <div>
+      <ul role="list" className="divide-y divide-gray-200">
+        {activityItems.map((activityItem) => (
+          <li key={activityItem.id} className="py-4">
+            <div className="flex space-x-3">
+              <img className="h-6 w-6 rounded-full" src={activityItem.person.imageUrl} alt="" />
+              <div className="flex-1 space-y-1">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-sm font-medium">{activityItem.person.name}</h3>
+                  <p className="text-sm text-gray-500">{activityItem.time}</p>
+                </div>
+                <p className="text-sm text-gray-500">
+                  Deployed {activityItem.project} ({activityItem.commit} in master) to {activityItem.environment}
+                </p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
           </Box>
           <Center height="100%" flex="1">
             <Box width="container.sm" pt="8" px="8">
