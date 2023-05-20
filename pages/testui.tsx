3f85f8ad53ed06d7ae55d35438926b736999957f
Admin <support@unlimitpotential.com>
2023-05-20 02:14:49 -0400

    hy

--- pages/testui.tsx
+++ pages/testui.tsx
@@ -1,59 +1,38 @@
-  import { Text,
+import { AppShell } from '@saas-ui/app-shell'
+import {
+    Sidebar,
+    SidebarSection,
+    NavItem,
+  } from '@saas-ui/sidebar'
+  import { Box, SimpleGrid,
   } from '@chakra-ui/react'
 
-  import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardBody,
-  Button,
-  Link,
-  CardFooter,
-} from '@saas-ui/react'
 
 
-export default function Home() {
+export default function Dash() {
 
 
   return (
     <>
-    <Card variant="outline">
-    <CardHeader>
-
-Logo      <CardTitle>Github</CardTitle>
-    </CardHeader>
-    <CardBody py={2}>
-      <Text color="muted" size="md">
-        Link pull requests
-      </Text>
-    </CardBody>
-    <CardFooter spacing={4}>
-      <Button variant="outline" colorScheme="green">
-        Enabled
-      </Button>
-      <Button as={Link} variant="link" isExternal>
-        Learn more
-      </Button>
-    </CardFooter>
-  </Card>
-  <Card variant="outline">
-    <CardHeader>
-Logo      <CardTitle>Slack</CardTitle>
-    </CardHeader>
-    <CardBody py={2}>
-      <Text color="muted" size="md">
-        Send push notifications
-      </Text>
-    </CardBody>
-    <CardFooter spacing={4}>
-      <Button colorScheme="gray" variant="solid">
-        Enable
-      </Button>
-      <Button as={Link} variant="link" isExternal>
-        Learn more
-      </Button>
-    </CardFooter>
-  </Card>
+     <AppShell
+  navbar={
+    <Box as="header" borderBottomWidth="1px" py="2" px="4">
+Logo    </Box>
+  }
+  sidebar={
+    <Sidebar>
+      <SidebarSection>
+        <NavItem>Home</NavItem>
+        <NavItem>Settings</NavItem>
+      </SidebarSection>
+    </Sidebar>
+  }
+>
+  <Box as="main" flex="1" py="2" px="4" overflowY="auto">
+  <SimpleGrid columns={[1, null, 2]} gap="2">
+  
+</SimpleGrid>  </Box>
+</AppShell>
     </>
   );
 }
