b5f0a90e8be5c01833ef4982199753b1f4569ecd
Admin <support@unlimitpotential.com>
2023-05-19 20:23:03 -0400

    s

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,22 +1,9 @@
 import { ReactNode } from 'react'
 import Head from 'next/head'
-import { Container } from '@chakra-ui/react'
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+import { Container, Flex, HStack, VisuallyHidden } from '@chakra-ui/react'
+import { Logo } from './Logo'
 
-
-const solutions = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#' },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#',  },
-  { name: 'Security', description: "Your customers' data will be safe and secure", href: '#' },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#',  },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#',  },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#',  },
-  { name: 'Contact sales', href: '#',  },
-]
-
+import { Link } from '@saas-ui/react'
 
 type Props = {
   children?: ReactNode
@@ -34,90 +21,25 @@ export const Layout = ({
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Container maxWidth="1200px">
-    <nav
-  aria-label="Site Nav"
-  className="mx-auto flex max-w-3xl items-center justify-between p-4"
->
-<Popover className="relative">
-      <Popover.Button className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-        <span>Solutions</span>
-        🚀
-      </Popover.Button>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
-      >
-        <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-          <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-            <div className="p-4">
-              {solutions.map((item) => (
-                <div key={item.name} className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-                  <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                  </div>
-                  <div>
-                    <a href={item.href} className="font-semibold text-gray-900">
-                      {item.name}
-                      <span className="absolute inset-0" />
-                    </a>
-                    <p className="mt-1 text-gray-600">{item.description}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-              {callsToAction.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100"
+      <header>
+        <Flex py={4} justifyContent="space-between" alignItems="center" mb={8}>
+          <Flex justifyContent="space-between" alignItems="center">
+            <nav>
+              <HStack spacing={15}>
+                <Link
+                  href="/"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
                 >
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </div>
-        </Popover.Panel>
-      </Transition>
-    </Popover>
-  <ul className="flex items-center gap-2 text-sm font-medium text-gray-500">
-    <li className="hidden lg:block">
-      <a className="rounded-lg px-3 py-2" href="/"> Home </a>
-    </li>
-
-    <li><a className="rounded-lg px-3 py-2" href=""> Projects </a></li>
-
-    <li>
-      <a
-        className="inline-flex items-center gap-2 rounded-lg px-3 py-2"
-        href=""
-        target="_blank"
-      >
-        External
-
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="h-4 w-4"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-          ></path>
-        </svg>
-      </a>
-    </li>
-  </ul>
-</nav>
+                  <Logo/>
+                  <VisuallyHidden>Saas UI Next.js starter</VisuallyHidden>
+                </Link>
+              </HStack>
+            </nav>
+          </Flex>
+Icon        </Flex>
+      </header>
       {children}
     </Container>
   </div>
--- components/layouts/logo.tsx
+++ components/layouts/logo.tsx
@@ -47,7 +47,6 @@ const Logo = ({ href = '/', onClick }: LogoProps) => {
  <Image
         src="https://res.cloudinary.com/unlimitpotential/image/upload/v1684541520/Screen_Shot_2023-05-19_at_8.09.24_PM_lhtctr.png"
         alt="Logo"
-        layout="fixed"
         height={75}
       />        <VisuallyHidden>{siteConfig.seo?.title}</VisuallyHidden>
       </AccessibleLink>
