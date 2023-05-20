98956d2e7315c8ab157347bec05a45b7f499d7dd
Admin <support@unlimitpotential.com>
2023-05-19 20:04:01 -0400

    s

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from 'react'
 import Head from 'next/head'
-import { Container, Flex, HStack, VisuallyHidden } from '@chakra-ui/react'
-import { Logo } from './Logo'
+import { Container } from '@chakra-ui/react'
 
-import { Link } from '@saas-ui/react'
 
 type Props = {
   children?: ReactNode
@@ -21,25 +19,51 @@ export const Layout = ({
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Container maxWidth="1200px">
-      <header>
-        <Flex py={4} justifyContent="space-between" alignItems="center" mb={8}>
-          <Flex justifyContent="space-between" alignItems="center">
-            <nav>
-              <HStack spacing={15}>
-                <Link
-                  href="/"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Logo/>
-                  <VisuallyHidden>Saas UI Next.js starter</VisuallyHidden>
-                </Link>
-              </HStack>
-            </nav>
-          </Flex>
-Icon        </Flex>
-      </header>
+    <nav
+  aria-label="Site Nav"
+  className="mx-auto flex max-w-3xl items-center justify-between p-4"
+>
+  <a
+    href="/"
+    className="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100"
+  >
+    <span className="sr-only">Logo</span>
+    👋
+  </a>
+
+  <ul className="flex items-center gap-2 text-sm font-medium text-gray-500">
+    <li className="hidden lg:block">
+      <a className="rounded-lg px-3 py-2" href="/"> Home </a>
+    </li>
+
+    <li><a className="rounded-lg px-3 py-2" href=""> Projects </a></li>
+
+    <li>
+      <a
+        className="inline-flex items-center gap-2 rounded-lg px-3 py-2"
+        href=""
+        target="_blank"
+      >
+        External
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          className="h-4 w-4"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+          ></path>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</nav>
       {children}
     </Container>
   </div>
