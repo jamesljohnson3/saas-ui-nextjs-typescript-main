5ec83adc705f2c5535c8badf75ccafcc3453698d
Admin <support@unlimitpotential.com>
2023-06-26 12:39:01 -0400

    j

---
+++ pages/pricing-test.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Pricing from "../components/ui/Pricing";
+import FAQs from "../components/ui/FAQs";
+import Navbar from "../components/ui/Navbar";
+import Footer from "../components/ui/Footer";
+
+export default function index() {
+  return (
+    <>
+      <Head>
+        <meta name='robots' content='index' />
+        <title>Pricing</title>
+      </Head>
+ 
+      <Pricing />
+      <FAQs />
+    </>
+  );
+}
